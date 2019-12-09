d8039f9891026893871f80189c19a60f99f91f57
Itallo Silva <itallosilva@yahoo.com.br>
2019-12-09 05:46:26 -0300

    Especificação do domínio no arquivo cors.php

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
         'allow_credentials' => false,
 
         'allow_origins' => [
-            '*',
+            'http://agendlaravel.herokuapp.com/',
         ],
 
         'allow_methods' => [
